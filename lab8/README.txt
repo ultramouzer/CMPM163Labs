4c2d10cfc4690114482307ac93bced9b44343134
Edmund Liang <eliang5@ucsc.edu>
2020-05-25 19:24:34 -0700

    finished

---
+++ lab8/README.txt
@@ -0,0 +1,13 @@
+Edmund Liang
+Lab 8
+
+Part 1
+
+My city: https://drive.google.com/file/d/11IQq_6NDzCrMK6beNOna-KEIWVcKKac1/view?usp=sharing
+Inspiration: https://drive.google.com/file/d/19XtsS6WNgXj81MdwFZPVsrGpdNayNzlT/view?usp=sharing
+
+My inspiration was an Animal Crossing town, so my design was affected by removing most large
+buildings, and replacing them with more trees, water, and benches. I also changed the height
+some objects were placed so that the water appeared lower than the land.
+
+I was not able to get in contact with my partner.
--- lab8/city/Assets/buildCity.cs
+++ lab8/city/Assets/buildCity.cs
@@ -17,20 +17,44 @@ public class buildCity : MonoBehaviour
             for(int w = 0; w < mapWidth; w++)
             {
                 int result = (int) (Mathf.PerlinNoise(w/10.0f + seed, h/10.0f + seed) * 10);
-                Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
+                Vector3 posG = new Vector3(w * buildingFootprint, 1, h * buildingFootprint);
                 if(result < 2)
-                    Instantiate(buildings[5], pos, Quaternion.identity);
+                {
+                    float randomx = Random.Range(-7, 7);
+                    float randomz = Random.Range(-7, 7);
+                    Vector3 pos2 = new Vector3((w * buildingFootprint) + randomx, 2, (h * buildingFootprint) + randomz);
+                    Instantiate(buildings[0], pos2, Quaternion.identity);//bench
+                    Instantiate(buildings[1], posG, Quaternion.identity);//grass
+                }
                 else if(result < 3)
-                    Instantiate(buildings[4], pos, Quaternion.identity);
+                {
+                    Instantiate(buildings[1], posG, Quaternion.identity);//grass
+                }
                 else if(result < 4)
-                    Instantiate(buildings[3], pos, Quaternion.identity);
+                {
+                    Vector3 posW = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
+                    Instantiate(buildings[2], posW, Quaternion.identity);//water
+                }
                 else if(result < 6)
-                    Instantiate(buildings[2], pos, Quaternion.identity);
+                {
+                    float randomx = Random.Range(-7, 7);
+                    float randomz = Random.Range(-7, 7);
+                    Vector3 pos2 = new Vector3((w * buildingFootprint) + randomx, 2, (h * buildingFootprint) + randomz);
+                    Instantiate(buildings[3], pos2, Quaternion.identity);//tree
+                    Instantiate(buildings[1], posG, Quaternion.identity);//grass
+                }
                 else if(result < 8)
-                    Instantiate(buildings[1], pos, Quaternion.identity);
+                {
+                    Vector3 pos3 = new Vector3(w * buildingFootprint, 2, h * buildingFootprint);
+                    Instantiate(buildings[4], pos3, Quaternion.identity);//small house
+                    Instantiate(buildings[1], posG, Quaternion.identity);//grass
+                }
                 else if(result < 10)
-                    Instantiate(buildings[0], pos, Quaternion.identity);
-                
+                {
+                    Vector3 pos3 = new Vector3(w * buildingFootprint, 2, h * buildingFootprint);
+                    Instantiate(buildings[5], pos3, Quaternion.identity);//big house
+                    Instantiate(buildings[1], posG, Quaternion.identity);//grass
+                }
             }
         }
     }
